26ca22aa559e87cc66f753848e20d2bc9f7eec62
Albert Ciesla <albert383@gmail.com>
2024-03-18 19:12:28 +0100

    corrected code in function

--- js/script.js
+++ js/script.js
@@ -1,46 +1,42 @@
-const Welcome = () => {
-    console.log("Witam wszystkich developerow, zmiana zapisana w repozytorium Git");
+{
+    const formSubmit = (event) => {
+        const formElement = document.querySelector(".js-form");
+        const inputAmountElement = document.querySelector(".js-amount");
+        const resultElement = document.querySelector(".js-result");
+        const eurElement = document.querySelector(".js-eur");
+        const chfElement = document.querySelector(".js-chf");
+        const usdElement = document.querySelector(".js-usd");
+
+        const convertToEUR = (amount) => {
+            return (amount * 0.231).toFixed(2) + "EUR";
+        };
+
+        const convertToCHF = (amount) => {
+            return (amount * 0.221).toFixed(2) + "CHF";
+        };
+
+        const convertToUSD = (amount) => {
+            return (amount * 0.251).toFixed(2) + "USD";
+        };
+
+        event.preventDefault();
+        if (!inputAmountElement.value) return;
+
+        let result = "";
+
+        if (eurElement.checked) {
+            result = convertToEUR(inputAmountElement.value);
+        }
+        if (chfElement.checked) {
+            result = convertToCHF(inputAmountElement.value);
+        }
+        if (usdElement.checked) {
+            result = convertToUSD(inputAmountElement.value);
+        }
+
+        resultElement.innerText = result;
+    };
+
+    const formElement = document.querySelector(".js-form");
+    formElement.addEventListener("submit", formSubmit);
 }
-
-Welcome();
-
-const formElement = document.querySelector(".js-form");
-const inputAmountElement = document.querySelector(".js-amount");
-const resultElement = document.querySelector(".js-result");
-
-const eurElement = document.querySelector(".js-eur");
-const chfElement = document.querySelector(".js-chf");
-const usdElement = document.querySelector(".js-usd");
-
-const convertToEUR = (amount) => {
-    return (amount * 0.231).toFixed(2) + "EUR";
-};
-
-const convertToCHF = (amount) => {
-    return (amount * 0.221).toFixed(2) + "CHF";
-};
-
-const convertToUSD = (amount) => {
-    return (amount * 0.251).toFixed(2) + "USD";
-};
-
-const formSubmit = (event) => {
-    event.preventDefault();
-    if (!inputAmountElement.value) return;
-
-    let result = "";
-
-    if (eurElement.checked) {
-        result = convertToEUR(inputAmountElement.value);
-    }
-    if (chfElement.checked) {
-        result = convertToCHF(inputAmountElement.value);
-    }
-    if (usdElement.checked) {
-        result = convertToUSD(inputAmountElement.value);
-    }
-
-    resultElement.innerText = result;
-};
-
-formElement.addEventListener("submit", formSubmit);
